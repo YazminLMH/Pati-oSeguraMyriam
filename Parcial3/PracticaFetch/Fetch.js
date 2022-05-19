218710e39eda7adf396e6c603718b49754db066a
YazminLMH <MyriamYazmin08@outlook.com>
2022-05-19 14:12:42 -0500

    correccion y practica fetch

--- Parcial3/PracticaFetch/Fetch.js
+++ Parcial3/PracticaFetch/Fetch.js
@@ -1,8 +1,22 @@
 
-$(document).ready(function () {
-$('#btnjson').click(function () {
-        $.post('BotonJson.php', {}, function (data) {
 
+$(document).ready(function () {
+    document.getElementById('btnFetch').addEventListener('click', function () {
+        let promise = fetch('Fetch.php');
+
+        promise.then(respuesta => respuesta.json())
+            .then(datos => console.log(datos));
+    });
+   
+   
+   
+    $('#btnjson').click(function () {
+        $.post('Fetch.php', {}, function (data) {
+
+           
+          
+          
+          
             console.log(data);
             $('#Numdecontrol').val(data.Num.decontrol);
             $('#Nombre').val(data.nombre);
@@ -16,3 +30,4 @@ $('#btnjson').click(function () {
         }, 'json');
     });
 });
+
